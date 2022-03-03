7f2d7efe66f61c42ef7c115dcbce9e29ea8a9f6c
EmilMis <77896233+EmilMis@users.noreply.github.com>
2022-03-03 22:53:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,9 @@
 
 using namespace std;
 typedef unsigned long long big;
-#define SQRT_MAGIC_F 0x5f3759df 
-
-float sqrt2(big x)
-{
-	const float xhalf = 0.5f * x;
-	union
-	{
-		float x;
-		int i;
-	} u;
-	u.x = x;
-	u.i = SQRT_MAGIC_F - (u.i >> 1);
-	return x * u.x * (1.5f - xhalf * u.x * u.x);
-}
 
 int prime(big number) {
-	int limit = round(sqrt2(number));
-	for (int i = 2; i <= limit; i++)if (number % i == 0)return 0;
+	for (int i = 2; i*i < number; i++)if (number % i == 0)return 0;
 	return 1;
 }
 
